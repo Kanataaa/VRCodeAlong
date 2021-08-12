db8c219f5a515078839d6a47b99b72ac01246ae1
Kanataaa <cqnfinedmc@gmail.com>
2021-08-12 15:46:49 -0400

    first repost:

--- script.js
+++ script.js
@@ -8,26 +8,32 @@ AFRAME.registerComponent('change-tree-color', {
 });
 
 
-AFRAME.registerComponent('change-color-hover', {
+AFRAME.registerComponent('change-color-on-hover', {
     init: function() {
         var snowHead = this.el;
 
-        snowHead.addEventListener('mouseeenter', function() {
+        snowHead.onmouseenter = function() {
             snowHead.setAttribute('color', 'purple');
-        })
+        }
 
-        snowHead.addEventListener('mouseleave', function() {
+        snowHead.onmouseleave = function() {
             snowHead.setAttribute('color', 'white');
-        })
+        }
     }
 });
 
 AFRAME.registerComponent('cursor-listener', {
     init: function(){
-        var colors = ['red', 'green', 'blue', 'yellow', 'purple']
-        var snowBody = this.el;
-        snowBody.addEventListener("click", function() { 
-            var randomIndex = Math.floor(Math.random * colors.length);
-        })
+       var colors = ['red', 'green', 'blue', 'yellow', 'purple']
+       var snowBody = this.el;
+       snowBody.addEventListener("click", function() {
+        var randomIndex = Math.floor(Math.random() * colors.length);
+        snowBody.setAttribute('color', colors[randomIndex]);
+       });
+       
+    // function changeColors() {
+           
+    //    }
     }
 });
+
